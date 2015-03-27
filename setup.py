d74cdd26431f584988b113b7c0c38f20c34e8ce2
Jonathan Kamens <jik@quantopian.com>
2015-03-27 08:58:52 -0400

    Bump version to 0.2.2.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info[0] < 3:
 
 setup(
     name="coal_mine",
-    version='0.2.1',
+    version='0.2.2',
     author='Quantopian Inc.',
     author_email='opensource@quantopian.com',
     description="Coal Mine - Periodic task execution monitor",
