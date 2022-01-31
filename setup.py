e67d3a9f446aaf32353fc624ed9bb15b38abccf0
Jonathan Kamens <jik@kamens.us>
2022-01-31 11:45:01 -0500

    Bump version number to 0.5.1.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class lazy_convert(object):
 
 setup(
     name="coal_mine",
-    version='0.5.0',
+    version='0.5.1',
     author='Quantopian Inc.',
     author_email='opensource@quantopian.com',
     description="Coal Mine - Periodic task execution monitor",
