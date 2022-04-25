558d444c84cdb4a6b550de30911e5880c375f66c
Jonathan Kamens <jik@kamens.us>
2022-04-25 03:16:53 -0400

    Increase version number to 0.6.0.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class lazy_convert(object):
 
 setup(
     name="coal_mine",
-    version='0.5.1',
+    version='0.6.0',
     author='Quantopian Inc.',
     author_email='opensource@quantopian.com',
     description="Coal Mine - Periodic task execution monitor",
