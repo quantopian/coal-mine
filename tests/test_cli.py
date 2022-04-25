bddd21f0fb6e2cc148a3c63aff31053cb6d8457e
Jonathan Kamens <jik@kamens.us>
2022-04-25 03:15:38 -0400

    Update copyrights
    Quantopian no longer exists and as far as I know I am the only person
    continuing to maintain this project, so I am reasserting copyright in
    my own name on the files I'm continuing to update.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 # Copyright 2015 Quantopian, Inc.
+# Copyright 2022 Jonathan Kamens
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you
 # may not use this file except in compliance with the License.  You
