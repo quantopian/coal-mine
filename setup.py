7036e9a8e722e33d6f010624f5110640d8ea3774
Jonathan Kamens <jik@kamens.us>
2024-08-14 13:57:11 -0400

    Increase version number to 0.7.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="coal_mine",
-    version='0.7',
+    version='0.7.1',
     author='Jonathan Kamens',
     author_email='jik+coalmine@kamens.us',
     description="Coal Mine - Periodic task execution monitor",
